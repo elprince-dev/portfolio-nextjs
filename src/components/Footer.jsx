c5448f76acf15c0c23ddcda671b2f4b127f64597
Mohamed Hussein <mohamedmhussein@protonmail.com>
2024-07-20 19:39:35 -0400

    Adjusts the footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,28 +8,20 @@ import {
 
 const Footer = () => {
   return (
-    <footer className="mx-auto max-w-3xl px-6 md:max-w-5xl">
+    <footer className="mx-auto max-w-4/5 px-6 ">
       <hr className="w-full h-0.5 mx-auto mt-8 bg-neutral-200 border-0" />
       <div className="mx-auto md:justify-between p-4 flex flex-col text-center text-neutral-900 md:flex-row">
         <div className="text-neutral-500 dark:text-neutral-100">
           © 2024 Mohammad El Prince
         </div>
         <div className="flex items-center justify-center space-x-2 mb-1">
-          <a
-            href="https://github.com/mohamedmhussein"
-            
-            target="_blank"
-          >
+          <a href="https://github.com/mohamedmhussein" target="_blank">
             <AiOutlineGithub
               className="hover:-translate-y-1 transition-transform cursor-pointer text-neutral-500 dark:text-neutral-100"
               size={30}
             />
           </a>
-          <a
-            href="https://www.linkedin.com/in/elprince93/"
-            
-            target="_blank"
-          >
+          <a href="https://www.linkedin.com/in/elprince93/" target="_blank">
             <AiOutlineLinkedin
               className="hover:-translate-y-1 transition-transform cursor-pointer text-neutral-500 dark:text-neutral-100"
               size={30}
